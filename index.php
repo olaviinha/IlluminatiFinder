78ceaa515269d34199caf2b76fa34f6339fe91b9
O. Inha <50331907+olaviinha@users.noreply.github.com>
2020-09-09 20:38:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,94 +1,94 @@
 
 <?php
 
-    if(isset($_POST['i']) && $_POST['i'] != ''){
+if(isset($_POST['i']) && $_POST['i'] != ''){
 
-        $img = $_POST['i'];     // Image URL
-        $top = $_POST['top'];   // Crop start from top
-        $left = $_POST['left']; // Crop start from left
-        $size = $_POST['size']; // Width & height from Crop top-left point
-        $frameSize = 600;       // Frame size (Final image is 2x2 frames)
+    $img = $_POST['i'];     // Image URL
+    $top = $_POST['top'];   // Crop start from top
+    $left = $_POST['left']; // Crop start from left
+    $size = $_POST['size']; // Width & height from Crop top-left point
+    $frameSize = 600;       // Frame size (Final image is 2x2 frames)
 
-        function processFrame($image, $startX, $startY, $width, $height, $frameSize, $drawTriangle) {
-            $imagick = new \Imagick($image);
-            $imagick->cropImage($width, $height, $startX, $startY);
-            $imagick->resizeImage($frameSize, $frameSize, imagick::FILTER_LANCZOS, true, true);
-            if($drawTriangle == true){
-                $draw = new \ImagickDraw(); 
-                $draw->setFillColor('red');
-                $draw->setStrokeColor('red');
-                $draw->setStrokeWidth(2);
-                $sy = rand(0, $frameSize/2);
-                $sx = rand(0, $frameSize/2);
-                $ey = rand($frameSize/4, $frameSize/1.2);
-                $ex = rand($frameSize/4, $frameSize/1.2);
-                $draw->line($sy, $sx, $ey, $ex);
-                $ney = rand(0, $frameSize);
-                $nex = rand(0, $frameSize);
-                $draw->line($ey, $ex, $ney, $nex);
-                $draw->line($ney, $nex, $sy, $sx);
-                $imagick->drawImage($draw);
-            }
-            return $imagick->getImageBlob();
+    function processFrame($image, $startX, $startY, $width, $height, $frameSize, $drawTriangle) {
+        $imagick = new \Imagick($image);
+        $imagick->cropImage($width, $height, $startX, $startY);
+        $imagick->resizeImage($frameSize, $frameSize, imagick::FILTER_LANCZOS, true, true);
+        if($drawTriangle == true){
+            $draw = new \ImagickDraw(); 
+            $draw->setFillColor('red');
+            $draw->setStrokeColor('red');
+            $draw->setStrokeWidth(2);
+            $sy = rand(0, $frameSize/2);
+            $sx = rand(0, $frameSize/2);
+            $ey = rand($frameSize/4, $frameSize/1.2);
+            $ex = rand($frameSize/4, $frameSize/1.2);
+            $draw->line($sy, $sx, $ey, $ex);
+            $ney = rand(0, $frameSize);
+            $nex = rand(0, $frameSize);
+            $draw->line($ey, $ex, $ney, $nex);
+            $draw->line($ney, $nex, $sy, $sx);
+            $imagick->drawImage($draw);
         }
+        return $imagick->getImageBlob();
+    }
 
-        function combine($firstFrame, $secondFrame, $rows) {
-            $im = new \Imagick();
-            $im->readImageBlob($firstFrame);
-            $im->readImageBlob($secondFrame);
-            $im->resetIterator();
-            $combined = $im->appendImages($rows);
-            $combined->setImageFormat("jpeg");
-            return $combined;
-        }
+    function combine($firstFrame, $secondFrame, $rows) {
+        $im = new \Imagick();
+        $im->readImageBlob($firstFrame);
+        $im->readImageBlob($secondFrame);
+        $im->resetIterator();
+        $combined = $im->appendImages($rows);
+        $combined->setImageFormat("jpeg");
+        return $combined;
+    }
 
-        $firstFrame = processFrame($img, $left, $top, $size, $size, $frameSize, false);
-        $secondFrame = processFrame($img, $left+($size/4), $top+($size/4), $size/2, $size/2, $frameSize, false);
-        $thirdFrame = processFrame($img, $left+($size/2), $top+($size/2), $size/4, $size/4, $frameSize, true);
-        $illuDough = processFrame('illudough.jpg', 0, 0, 800, 800, $frameSize, false);
+    $firstFrame = processFrame($img, $left, $top, $size, $size, $frameSize, false);
+    $secondFrame = processFrame($img, $left+($size/4), $top+($size/4), $size/2, $size/2, $frameSize, false);
+    $thirdFrame = processFrame($img, $left+($size/2), $top+($size/2), $size/4, $size/4, $frameSize, true);
+    $illuDough = processFrame('illudough.jpg', 0, 0, 800, 800, $frameSize, false);
 
-        $firstRow = combine($firstFrame, $secondFrame, false);
-        $secondRow = combine($thirdFrame, $illuDough, false);
+    $firstRow = combine($firstFrame, $secondFrame, false);
+    $secondRow = combine($thirdFrame, $illuDough, false);
+    $finale = combine($firstRow, $secondRow, true);
 
-        $finale = combine($firstRow, $secondRow, true);
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+    header('Content-Type: image/jpeg');
+    echo base64_encode($finale);
 
-        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-        header("Cache-Control: post-check=0, pre-check=0", false);
-        header("Pragma: no-cache");
-        header('Content-Type: image/jpeg');
-        echo base64_encode($finale);
-
-    } else {
+} else {
 
 ?>
 
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="description" content="Illuminati Finder">
-        <meta name="author" content="O. Inha">
-        <title>Illuminati Finder</title>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="cropper.min.js"></script>
-        <link  href="cropper.min.css" rel="stylesheet" />
-        <script src="jquery.cropper.min.js"></script>
-    </head>
-    <body>
-        <div class="cropper">
-            <div class="start">
-                <img src="pilluminati.png" class="logo" />
-                <input type="text" id="imgUrl" placeholder="Paste image URL here." />
-            </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Illuminati Finder">
+    <meta name="author" content="O. Inha">
+    <title>Illuminati Finder</title>
+    <link href="//fonts.googleapis.com/css2?family=Gayathri:wght@100;400&display=swap" rel="stylesheet" />
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="cropper.min.js"></script>
+    <link  href="cropper.min.css" rel="stylesheet" />
+    <script src="jquery.cropper.min.js"></script>
+</head>
+<body>
+    <div class="cropper">
+        <div class="start">
+            <img src="pilluminati.png" class="logo" />
+            <input type="text" id="imgUrl" placeholder="Paste image or image URL here." />
         </div>
-        <div class="actions"><button id="submit">Find Illuminati</button></div>
-        <link rel="stylesheet/less" type="text/css" href="pilluminati.less" />
-        <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.2/less.min.js"></script>
-        <script src="pilluminati.js"></script>
-    </body>
+    </div>
+    <div class="actions"><button id="submit">Find Illuminati</button></div>
+    <link rel="stylesheet/less" type="text/css" href="pilluminati.less" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.2/less.min.js"></script>
+    <script src="pilluminati.js"></script>
+</body>
 </html>
 
 <?php
-    }
+}
 ?>
 
 
